a7685867aca480840308ae355846c8d5aee04755
RokayaAhmedSharawy <roka.sharawy2020@gmail.com>
2026-01-27 19:24:44 +0200

    modify sign in page to link with Backend

--- src/pages/Sign_In.js
+++ src/pages/Sign_In.js
@@ -1,15 +1,51 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Sign_In.css'; 
-import logo from '../photos/logo.png'; 
+import { Link, useNavigate } from 'react-router-dom'; 
+import './Sign_In.css';
+import logo from '../photos/logo.png';
 
 function SignIn() {
     const [showPassword, setShowPassword] = useState(false);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const navigate = useNavigate();
+
     const togglePasswordVisibility = () => {
         setShowPassword(!showPassword);
+    };
+
+    // 3. backenddddddddd
+    const handleSubmit = async (e) => {
+        e.preventDefault(); // No Refresh
+
+        try {
+            // send data to backend
+            const response = await fetch('http://localhost:5000/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password }), // data sent
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                // if data right and the respose of server is OK
+                alert("logged in successfully");
+
+                
+                // localStorage.setItem('user', JSON.stringify(data));
+
+                navigate('/'); // go to home
+            } else {
+                // if there is any problem like wrong email or password)
+                alert(data.message || "failed to login ,ensure your data");
+            }
+        } catch (error) {
+            console.error("Error:", error);
+            alert("failed to connect server");
+        }
     };
 
     return (
@@ -22,14 +58,14 @@ function SignIn() {
 
             <div className="signin-card">
                 <div className="logo-container">
-                    
                     <img src={logo} alt="CareerNode" className="signin-logo" />
                 </div>
 
                 <h2>Welcome Back</h2>
                 <p className="subtitle">Enter your credentials to access your account</p>
 
-                <form className="signin-form">
+                {/* 4.link betwwen function and Form */}
+                <form className="signin-form" onSubmit={handleSubmit}>
                     <div className="form-group">
                         <label>Email</label>
                         <input
@@ -37,6 +73,7 @@ function SignIn() {
                             placeholder="Enter your email"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
+                            required 
                         />
                     </div>
 
@@ -48,6 +85,7 @@ function SignIn() {
                                 placeholder="Enter your password"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
+                                required
                             />
 
                             <span className="eye-icon" onClick={togglePasswordVisibility}>
